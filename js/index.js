f27b892300baeeb9332d7331b481845bb0b7fb28
Joon <jong@jongs-mbp.lan>
2020-09-01 19:50:01 -0700

    finally add preventDefault() and propagation

--- js/index.js
+++ js/index.js
@@ -25,7 +25,8 @@ const navItems = document.querySelectorAll('.nav-link')
 navItems.forEach( x => {
     x.addEventListener('mouseenter', () => {
         x.style.transform = 'scale(1.1)';
-        x.style.transition = '0.2s'
+        x.style.transition = '0.3s'
+        x.style.color = 'white'
     })
 })
 
@@ -35,34 +36,26 @@ navItems.forEach( x => {
     x.addEventListener('mouseleave', () => {
         x.style.transform = 'scale(1)';
         x.style.transition = '0.2s'
+        x.style.color = 'black'
     })
 })
 
 // 5 (Double Click)
 
-document.addEventListener('dblclick', (e) => {
+document.addEventListener('dblclick', e => {
     e.target.style.transform = 'scale(1.1)';
     e.target.style.transition = '0.2s'
 })
 
 // 6 (Mouse Out)
 
-document.addEventListener('mouseout', (e) => {
+document.addEventListener('mouseout', e => {
     e.target.style.transform = 'scale(1)'
     e.target.style.transition = '0.2s'
 })
 
 // 7 (Key Down)
 const signMe = document.querySelectorAll('.btn');
-
-// signMe.forEach( x => {
-//     x.addEventListener('keydown', () => {
-//         x.style.color= 'scale(1.1)'
-//         x.style.transition = '0.2s'
-//         console.log(x)
-//     })
-// })
-
 document.addEventListener('keydown', () => {
     signMe.forEach( x => {
         x.style.transform= 'scale(1.1)'
@@ -71,9 +64,81 @@ document.addEventListener('keydown', () => {
 })
 
 // 8 (Key Up)
-document.addEventListener('keyup', () => {
+window.addEventListener('keyup', () => {
     signMe.forEach( x => {
         x.style.transform= 'scale(1)'
         x.style.transition = '0.2s'
+    })
+})
+
+// 9 (Scroll)
+const paragraph = document.querySelectorAll('p')
+window.addEventListener('scroll', () => {
+    paragraph.forEach( x => {
+        x.style.color = `rgb(${Math.random() * 255 + 1}, ${Math.random() * 255 + 1}, ${Math.random() * 255 + 1})`
+    })
+})
+
+//10 (Resize)
+const imgs = document.querySelectorAll('img');
+window.addEventListener('resize', () => {
+   imgs.forEach( x => {
+       if(x.style.opacity === '0.5'){
+           x.style.opacity = '1';
+       } else {
+           x.style.opacity = '0.5';
+       }
+    })
+})
+
+// footer
+const foot = document.querySelector('.footer');
+
+// Create Mom and Kid box
+const box = document.createElement('div');
+const boxTwo = document.createElement('div');
+
+// Mom box CSS
+foot.style.display = 'flex';
+foot.style.justifyContent = 'space-around'
+box.textContent = 'Mom'
+box.style.border = 'solid 2px blue';
+box.style.padding = '2%'
+box.style.width = '15%'
+box.style.border = 'solid 2px blue';
+box.style.display = 'flex'
+box.style.justifyContent = 'center'
+
+// Kid box CSS
+boxTwo.textContent = 'Kid'
+boxTwo.style.border = 'solid 2px red';
+boxTwo.style.padding = '2%'
+boxTwo.style.textAlign = 'center'
+boxTwo.style.width = '50%'
+
+//appending two elements (Mom and Kid)
+foot.appendChild(box)
+box.appendChild(boxTwo)
+
+// Prevent Propagation
+foot.addEventListener('click', e => {
+    e.stopPropagation();
+    foot.style.backgroundColor = 'lightblue'
+})
+
+box.addEventListener('click', e => {
+    e.stopPropagation();
+    box.style.backgroundColor = 'green'
+})
+
+boxTwo.addEventListener('click', e => {
+    e.stopPropagation();
+    boxTwo.style.backgroundColor = 'orange'
+})
+
+// preventDefault() at navigation Items from default 'click' events;
+navItems.forEach( eachItems => {
+    eachItems.addEventListener('click', e => {
+        e.preventDefault();
     })
 })
